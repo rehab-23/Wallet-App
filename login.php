de1c7ca31f862ba4ed85f8df967642bbe68079d1
rehab-23 <habermann-r@web.de>
2026-04-16 08:30:47 +0200

    header.php erstellt und integriert

--- login.php
+++ login.php
@@ -8,11 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wallet-App</title>
-</head>
+<?php include 'header.php'; ?>
 
 <body>
     <div style="display: flex; align-items: center; justify-content: center;">
