9b84e92184db9162b41efa23cde9446988297764
rehab-23 <habermann-r@web.de>
2026-04-16 10:38:42 +0200

    header angepasst

--- header.php
+++ header.php
@@ -1,5 +1,7 @@
 <header>
     <div style="display: flex; align-items: center; justify-content: center;">
-        <h1>Wallet-App</h1>
+        <h1>
+            <name style="font-style:italic;">your</name> Wallet-App
+        </h1>
     </div>
 </header>
--- index.php
+++ index.php
@@ -7,10 +7,7 @@
     <?php include 'header.php'; ?>
     <div style="display: flex; align-items: center; justify-content: center;">
         <div>
-            <h1>
-                <name style="font-style:italic;">your</name> Wallet-App
-            </h1>
-            <hr>
+            <h1>- Landingpage -</h1>
             <p><a href="register.php">Zur Registrierung</a></p>
             <p><a href="login.php">Zum Login</a></p>
         </div>
