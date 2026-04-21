d8f51e1e14471fc49b7d5702a3275ca3b77a86a0
rehab-23 <habermann-r@web.de>
2026-04-21 12:03:35 +0200

    Code-Refactoring: HTML-Code

--- register.php
+++ register.php
@@ -36,11 +36,7 @@
                     <input type="password" class="form-control" id="passwordeingabe" name="password_wdh"
                         placeholder="password" required="">
                 </div>
-                <div class="mb-3 form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                </div>
-                <input type="submit" class="btn btn-primary" name="register_btn" value="registrieren"></input><br><br>
+                <input type="submit" class="btn btn-primary" name="register_btn" value="registrieren"></input><br>
             </form>
             <br>
             <?php echo buttonblau("index.php", "zurück"); ?>
