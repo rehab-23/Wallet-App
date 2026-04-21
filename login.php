3685b6b70f51f12dfb99827ba58ce99b0f4c230e
rehab-23 <habermann-r@web.de>
2026-04-21 09:40:58 +0200

    Formulare erneuert mit Bootstrap

--- login.php
+++ login.php
@@ -4,23 +4,35 @@
 
     echo einloggen($config);
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'; ?>
 
 <body>
     <?php include 'header.php'; ?>
+
     <div style="display: flex; align-items: center; justify-content: center;">
         <div>
             <h1>- Login -</h1>
             <form action="" method="POST">
-                <label for="">Email eingeben:</label><br>
-                <input type="email" name="email" placeholder="email" required=""><br><br>
-                <input type="password" name="passwordeingabe" placeholder="password" required=""><br><br>
-                <input type="submit" name="login_btn" value="login"><br><br>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email eingeben</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email"
+                        placeholder="email" required="">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label for="passwordeingabe" class="form-label">Passwort eingeben</label>
+                    <input type="password" class="form-control" id="passwordeingabe" name="passwordeingabe"
+                        placeholder="password" required="">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary" name="login_btn" value="login">Submit</button><br><br>
             </form>
-            <?php echo buttonblau("index.php", "zurück"); ?>
+            <?php echo buttonblau("home.php", "zurück"); ?>
         </div>
     </div>
     <?php include 'footer.php'; ?>
